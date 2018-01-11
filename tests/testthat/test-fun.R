4d774a7dafe27306e3e3908e563b9b5a01f58900
amrrs <amrrs@outlook.com>
2018-01-11 14:10:41 +0530

    adding test case get_last31days_price

--- tests/testthat/test-fun.R
+++ tests/testthat/test-fun.R
@@ -14,3 +14,11 @@ test_that("The output dataframe Type is ",{
         expect_equal(class(get_current_price()),"data.frame")
 
 })
+
+context("get_last31days_price Output DF Check")
+
+test_that("The output dataframe Type is ",{
+
+        expect_equal(class(get_last31days_price()),"data.frame")
+
+})
